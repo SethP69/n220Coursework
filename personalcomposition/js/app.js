819e7133e6b2d6d9aa26c8d15a44b63e631395e1
SethP69 <89537431+SethP69@users.noreply.github.com>
2021-09-01 15:37:39 -0400

    Initial Project

---
+++ personalcomposition/js/app.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400,400)
+}
+
+function draw() {
+    fill(24, 210, 132)
+    background(211)
+    ellipse(50,50,50,50)
+    stroke(200, 132, 192)
+    rect(200,200,80,100)
+    fill (210, 123, 14)
+}
